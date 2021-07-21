22075a297eec18fc2d15530b65b3cf040407e8e7
Diego <diegopaginidev@gmail.com>
2021-07-21 08:44:50 -0300

    cards

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Stage } from '../models/stage.interface';
 
 @Component({
@@ -11,26 +12,39 @@ export class DashboardComponent implements OnInit {
     {
       id: 1,
       name: 'Backlog',
-      cards: [],
+      cards: [1, 2, 3],
     },
     {
       id: 2,
       name: 'To Do',
-      cards: [],
+      cards: ['asda', 'dasdasdasdasd'],
     },
     {
       id: 3,
       name: 'Ongoing',
-      cards: [],
+      cards: ['asdasddwq'],
     },
     {
       id: 4,
       name: 'Done',
-      cards: [],
+      cards: ['mukdlas'],
     },
   ];
+  public taskCreatorForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.taskCreatorForm = this.fb.group({
+      task: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {}
+
+  addTask() {
+    if (this.taskCreatorForm.valid) {
+      this.taskCreatorForm.reset();
+    } else {
+      this.taskCreatorForm.markAllAsTouched();
+    }
+  }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [DashboardComponent],
 })
 export class DashboardModule {}
